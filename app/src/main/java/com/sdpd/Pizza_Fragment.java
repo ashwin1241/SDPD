584cc0a566e06bd5218ea7e0d2d6c2fe34b3b10b
ashwin1241 <ashwinwadatkar@gmail.com>
2021-11-12 15:58:32 +0530

    Lab 8

---
+++ app/src/main/java/com/sdpd/Pizza_Fragment.java
@@ -0,0 +1,22 @@
+package com.sdpd;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Pizza_Fragment extends Fragment {
+
+    private ListView pizza_list;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pizza_fragment,container,false);
+    }
+}
